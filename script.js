f8ee4530f0dbe6435a113be19005d23ab40fffa6
Gerhard <gerhard.putter@yahoo.com>
2021-09-16 04:39:43 +0200

    Add functionality of modal.

--- script.js
+++ script.js
@@ -114,3 +114,76 @@ document.querySelector(".right-btn").addEventListener("click", () => {
 menu.addEventListener("click", () => {
   document.querySelector(".navbar").classList.toggle("change");
 });
+
+
+// MODAL
+
+//Form  Validation
+
+//Variables
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const passwordmatch = document.getElementById('passwordmatch');
+
+//Functions
+function showError(input, message) {
+  const Register = input.parentElement;
+  Register.className = "register error"
+  const small = Register.querySelector('small')
+  small.innerText = message
+}
+
+function showSuccess(input) {
+  const Register = input.parentElement;
+  Register.className = "register success"
+
+}
+
+//Check username
+function validateUsername(username) {
+  const re = /^[A-Za-z]+$/
+  return re.test(String(username).toLowerCase());
+}
+//Check email
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+//Evenlisteners
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (username.value === "") {
+    showError(username, 'Username is required')
+  } else if (!validateUsername(username.value)) {
+    showError(username, 'Username is not valid. Numbers not allowed')
+  }
+  else {
+    showSuccess(username)
+  }
+  if (email.value === "") {
+    showError(email, 'Error Email is required')
+  } else if (!validateEmail(email.value)) {
+    showError(email, 'Email is not valid')
+  } else {
+    showSuccess(email)
+  }
+  if (password.value === "") {
+    showError(password, 'Error Password is required')
+  } else {
+    showSuccess(password)
+  }
+
+  if (passwordmatch.value === "") {
+    showError(passwordmatch, 'Error Password is required')
+  }
+  else if (passwordmatch.value !== password.value) {
+    showError(passwordmatch, 'Error Passwords should match')
+  } else {
+    showSuccess(passwordmatch)
+  }
+});
